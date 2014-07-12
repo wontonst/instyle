dc0d15dcc734ce6127d42ed4ca96bc6e36fd1df5
Nicole Calace <ncalace@ncalace-mn1.linkedin.biz>
2014-07-12 02:48:53 -0700

    nicole pull in

--- src/view/profile.php
+++ src/view/profile.php
@@ -1,1 +1,68 @@
-add profile page here
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>[IN]Style</title>
+    <link rel="stylesheet" href="../css/foundation.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div class="navbar">
+		<ul>
+			<li><img id="logo" src="../images/instyle-logo.svg"></li>
+			<li>blah</li>
+			<li>blah</li>
+			<li>blah</li>
+			<li>blah</li>
+			<li>blah</li>
+
+		</ul>
+	</div>
+
+	<div class="maincontent">
+
+	<div class="row titlecard">
+		<div class="large-3 columns">
+        	<img class="leftcard" src="../images/justin3.jpeg">
+    	</div>
+
+    	<div class="large-9 columns">
+    		<div class="maincard">
+    			<span class="rightcard">
+	        		<h1>Justin Beiber</h1>
+	        		<h2>Pop singer and song writer</h2>
+	        		<h3>Ontario, Canada | Music</h3>
+	        	</span>
+        	</div>
+    	</div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+    	<div class="large-12 columns" id="help">
+    		<h2>Help Justin pick out a new LinkedIn profile picture</h2>
+    	</div>
+    </div>
+
+    <div class="row">
+    	<div class="large-12 columns" id="help">
+    		<ul class="small-block-grid-1 large-block-grid-4">
+			  <li> <img src="../images/justin2.jpeg"> <span class="imglabel">12 Votes  <span class="hr">|</span> <a href=""> <img class="thumbup" src="../images/thumb-icon.svg"> </a> </span> </li>
+
+			  <li> <img src="../images/justin.jpeg"> <span class="imglabel">12 Votes  <span class="hr">|</span> <a href=""> <img class="thumbup" src="../images/thumb-icon.svg"> </a> </span> </li>
+
+			  <li> <img src="../images/justin4.jpeg"> <span class="imglabel">12 Votes  <span class="hr">|</span> <a href=""> <img class="thumbup" src="../images/thumb-icon.svg"> </a> </span> </li>
+
+			  <li> <img src="../images/justin5.jpeg"> <span class="imglabel">12 Votes  <span class="hr">|</span> <a href=""> <img class="thumbup" src="../images/thumb-icon.svg"> </a> </span> </li>
+			  
+			</ul>
+    	</div>
+  	</div>
+</body>
+
+</html>
