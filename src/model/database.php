2d1564503e5c49b2cc4814f97d80251f0347b245
Roy Zheng <roycraft3@gmail.com>
2014-07-11 20:20:14 -0700

    database frame

--- src/model/database.php
+++ src/model/database.php
@@ -1,13 +1,41 @@
 <?php
-
-function getRandom(){
+/**
+@param myId id of the user requesting. do NOT return the user's own picture :D
+@returns an array in this fashion
+array(
+[0]=>array(
+[url]=>'string of the image',
+[up]=>int number of up votes
+),
+);
+*/
+function getImg($myId=-1){
 
 }
-function createAccount($email,$pwd){
+/**
+@param user adding the image, the image itself is passed from the form. Communicate with Nicole on what the $_GET is
+*/
+function addImg($myId){
 
 }
-function rateUp($url){
+/**
+@param id the id of the img to drop
+*/
+function dropImg($id){
 
 }
+/**
+@param id the id of the image to rate up
+*/
+function rateUp($id){
+
+}
+function register($email,$pwd){
+
+}
+function login($email,$pwd){
+
+}
+
 
 ?>
