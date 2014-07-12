8a79860ab87af9fcb83acfb18268c75bf86783ba
yglazko <yglazko@yglazko.local>
2014-07-12 01:33:21 -0700

    Changed home.php to correct front-end

--- src/view/home.php
+++ src/view/home.php
@@ -1,1 +1,45 @@
-put home page here
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+    <div class="row">
+      <div class="large-12 columns" style ="background-color:#305B82; font-family: 'Roboto Slab', serif;">
+        <div class = "row"> <div class = "large-10 columns"><h1 style="color:#FFF; font-family: 'Roboto Slab', serif;">InStyle</h1> </div><div class = "large-2 columns" style = "padding-top:.5cm;"> <a href="#" class="button success">Log In</a></div></div></div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="large-3 columns" style="padding-top:1cm; background-color:#CCC;">
+	        <form data-abide> <div class="name-field"> <label>Your name <small>required</small> <input type="text" required pattern="[a-zA-Z]+"> </label> <small class="error">Name is required and must be a string.</small> </div> <div class="email-field"> <label>Email <small>required</small> <input type="email" required> </label> <small class="error">An email address is required.</small> </div> <div class="password-field"> <label>Password <small>required</small> <input type="password" required> </label> <small class="error">A password is required.</small> </div><button type="submit">Submit</button> </form>
+      </div>
+       <div class="large-8 columns" style="padding-top:1cm;">
+          <div class = "row">
+          <div class = "large-12 columns">
+          <font size="6" style="font-family: 'Indie Flower', cursive; font-weight: 400; font-size: 72; text-align:center " align="center">  Need help choosing a professional picture? </font> 
+          </div></div> </br>
+
+
+          <ul class="example-orbit" data-orbit> <li> <div class = "row"><div class = "large-12 columns"><div class = "row"><div class = "large-5 columns"><img src="img/homepage.jpg" style ="border:10px solid black" alt="Smiley face"></div><div class = "large-2 columns"><font size="6" style="font-family: 'Indie Flower', cursive; font-weight: 400; font-size: 72; padding-top:4cm;">  Vs.  </font></div><div class = "large-5 columns"> <img src="img/homepage2.jpg" style ="border:10px solid black" alt="Smiley face"></div></div></div> </div> </li> <li class="active"><div class = "row"><div class = "large-12 columns"><img src="img/lnstyle.jpg"></div> </div></li> </ul>
+
+
+      </div>
+    </div>
+
+    
+    </div>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Indie+Flower|Roboto+Slab' rel='stylesheet' type='text/css'>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
